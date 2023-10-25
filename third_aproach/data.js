0d24f47ba8c057d2f0973ad6051b6accc60d87c7
hivancd <111187629+hivancd@users.noreply.github.com>
2023-10-25 18:37:24 -0500

    data.js_creates_geojson

--- third_aproach/data.js
+++ third_aproach/data.js
@@ -2,8 +2,6 @@ let template = require('./countries.json');
 
 let database = require('../data/data.json');
 
-let countries_t = (template.features);
-
 // console.log(all_data.countries.ven);
 
 function get_element_by_id(obj, id_val) {
@@ -11,11 +9,13 @@ function get_element_by_id(obj, id_val) {
 }
 
 
-function get_geojson(countries_t,database){
+function get_feautures(template,database){
+
+    let countries_t = (template.features);
     countries_t.forEach(countrie => {
         countrie.id = countrie.id.toLowerCase();
         let id = get_element_by_id(database.countries, countrie.id);
-        if (id != null) {
+        if (id != undefined) {
             let properties = countrie.properties;
 
             // Add base properties
@@ -23,19 +23,38 @@ function get_geojson(countries_t,database){
 
             // Add sports properties
             const sports = database.sports;
-            properties.sports
+            properties.sports = {};
+
             Object.keys(sports).forEach(sport => {
-                let female = sports[sport][female][countries].find(id);
-                let male = sports[sport][male][countries].find(id);
-                // properties.
+                let female = sports[sport].female.countries.find(e => e.country_id == id);
+                let male = sports[sport].male.countries.find(e => e.country_id == id);
+
+                properties.sports[sport] = {"male":"","female":""}
+
+                properties.sports[sport].male = 'No participation';
+                properties.sports[sport].female = 'No participation';
+
+                if(male != null){properties.sports[sport].male = male;}
+                if(female!=null){properties.sports[sport].female = female;}
             });
+            countrie.properties = properties;
         }
         else {
             countrie.properties.no_data = 'No data';
         }
     });
+    return countries_t;
 }
 
+function get_geojson(template,database) {
+    const features = get_feautures(template,database);
+    // console.log(features);
+    let geojson = (template);
+    geojson.features = features;
+    return geojson;
+}
+
+let geojson = get_geojson(template,database);
 
 
 
